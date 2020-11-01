8400097ae6cd20a8296465f1495b8a65297bfb96
Akram Ouardas <a.ouardas@esi-sba.dz>
2020-11-02 00:15:22 +0100

    first commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Aux from '../../../src/hoc/auxilary'
+class BurgerBuilder extends Component {
+    // this is a test for github initalization
+    render(){
+        return( 
+            <Aux>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </Aux>
+        ); 
+
+    }
+
+}
+
+export default BurgerBuilder
